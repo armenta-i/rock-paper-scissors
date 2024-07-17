d91de4ee51d9c1340dcb5b4476a398c35ac24092
armenta-i <i_armenta@outlook.com>
2024-07-17 15:29:50 -0500

    Added function to compare the two input from the player and computer and ability to play a round

--- script.js
+++ script.js
@@ -21,5 +21,58 @@ function getPlayerChoice() {
     return userInput;
 }
 
+function compareChoice(computerChoice, playerChoice){
+    let biggerScore;
+    
+    // 0 means they tied, 1 means computer won, and 2 means player won
+    switch(true) {
+        case (computerChoice === playerChoice):
+            biggerScore = 0;
+            break;
+        case (computerChoice === 'rock' && playerChoice === 'scissors'):
+            biggerScore = 1;
+            break;
+        case (computerChoice === 'paper' && playerChoice === 'rock'):
+            biggerScore = 1;
+            break;
+        case (computerChoice === 'scissors' && playerChoice ==='paper'):
+            biggerScore = 1;
+            break;
+        case (computerChoice === 'rock' && playerChoice === 'paper'):
+            biggerScore = 2;
+                break;
+        case (computerChoice === 'paper' && playerChoice === 'scissors'):
+            biggerScore = 2;
+            break;
+        case (computerChoice === 'scissors' && playerChoice === 'rock'):
+            biggerScore = 2;
+            break;
+    }
+    return biggerScore;
+}  
 
+
+function playRound(computerChoice, playerChoice) {
+    let winner = compareChoice(computerChoice, playerChoice);
+
+    switch(winner){
+        case(0):
+            console.log("Player and Computer had same choice, no winner");
+            break;
+        case(1):
+            console.log("You lose! " + computerChoice + " beats " + playerChoice);
+            computerScore++;
+            break;
+        case(2):
+            console.log("You win! " + playerChoice + " beats " + computerChoice);
+            playerScore++;
+            break;
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let computerDecision = getComputerChoice();
+let playerDecision = getPlayerChoice();
+playRound(computerDecision, playerDecision);
 
