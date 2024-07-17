945b0ee8d0982217705a945f5df206ccc45c8b98
armenta-i <i_armenta@outlook.com>
2024-07-17 15:05:55 -0500

    Added functions to generate computer choice and get user choice to play

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello")
+function getComputerChoice() {
+    let randomIndex = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+    switch(randomIndex){
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break;
+    }
+    return computerChoice;
+}
+
+function getPlayerChoice() {
+    let userInput = prompt("Choose one of the options to play against you opponent!\n Rock Paper Scissors\n 0 to exit");
+    userInput = userInput.toLowerCase();
+    return userInput;
+}
+
+
+
